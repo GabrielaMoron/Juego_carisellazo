9dd8ab38cecd77381c1296b41bb7b4995c6f499d
GabrielaMoron <104436274+GabrielaMoron@users.noreply.github.com>
2022-11-03 11:52:51 -0500

    add color

--- Carisellazo/js/juego.js
+++ Carisellazo/js/juego.js
@@ -4,46 +4,41 @@ let cont=0;
 let total=0;
 let totalapuesta=0
 let moneda = document.querySelector(".moneda");
-let girarBtn = document.querySelector("#flip-button");
-let reinicioBtn = document.querySelector("#reinicio-button");
+let girarBtn = document.querySelector("#jugar");
+let reinicioBtn = document.querySelector("#volver");
 let apuestaInput = document.querySelector("#apuest");
 let opcionInput = document.querySelector("#opcion");
 
-//hacer girar la monedad
+
 
 girarBtn.addEventListener("click", () => {
-    //validacion de campos
+   
     if(apuestaInput.value !='' && opcionInput.value!=''){
 
-       do{
+        do{
         let i = Math.floor(Math.random() * 2);
         cont++;
 
     moneda.style.animation = "none";
-    //almacena la opcion del usuario
     let opcUsuario=parseInt(opcionInput.value)
-    //almacena lo que gano por apuesta
     let apuestaUsu=parseInt(apuestaInput.value)
-    //total del dinero que aporto
     totalapuesta+=apuestaUsu;
     
     if(i){
-        //animacion de la moneda con duracion de 2 segundos
-        moneda.style.animation = "spin-cara 2s forwards";
-        setTimeout(function(){
-            //si la opcion del usuario es igual al del random
-            if(i===opcUsuario){
-                total=apuestaUsu+=1000;
-                swal(`Felicidades, ganaste la moneda cayo en cara. ganaste $1000. en total tienes: ${total}`);
-                
-                //sino 
-            } else{
-                total=apuestaUsu-=1000;
-                swal(`lo sentimos, has perdido la moneda cayo en cara. perdiste $1000 en total tienes: ${total}`);
-              
-            }
-        }, 3000);
-        cara++;
+            moneda.style.animation = "spin-cara 2s forwards";
+             setTimeout(function(){
+                                
+             if(i===opcUsuario){
+                                        total=apuestaUsu+=1000;
+                                        swal(`Felicidades, ganaste la moneda cayo en cara. ganaste $1000. en total tienes: ${total}`);
+                            
+                                    } else{
+                                        total=apuestaUsu-=1000;
+                                        swal(`lo sentimos, has perdido la moneda cayo en cara. perdiste $1000 en total tienes: ${total}`);
+                                    
+                                    }
+                                }, 3000);
+                                cara++;
 
     }
     else{
@@ -63,9 +58,8 @@ girarBtn.addEventListener("click", () => {
     }
     setTimeout(updateStats, 3000);
     disableButton();
-    }while(total ===0 ){  swal('Upss, ya no tienes dinero, ingresa dinnero a tu apuesta')}
+    }while(total <=0){ swal('Ups!,ya no tienes dinero')}
 }
-    
     else{
         swal('Selecciona una opcion para jugar y un valor a apostar')
     }
